9f79868abcf7afbf54399ab909f504ef5cf2b057
Randall Adams <rjadams96@gmail.com>
2021-06-23 21:37:15 -0600

    add i18n

--- src/main/java/com/randalladams/scheduler/login/MainApp.java
+++ src/main/java/com/randalladams/scheduler/login/MainApp.java
@@ -6,12 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 
 public class MainApp extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+    // get and set the locale
+    String lang = System.getProperty("user.language");
+    Locale locale = new Locale(lang, lang.toUpperCase());
+    ResourceBundle bundle = ResourceBundle.getBundle("i18n", locale);
+    FXMLLoader fxmlLoader = new FXMLLoader();
+    Parent root = fxmlLoader.load(getClass().getResource("/fxml/login.fxml"), bundle);
     stage.setTitle("Login");
     stage.setScene(new Scene(root, 800, 500));
     stage.show();
