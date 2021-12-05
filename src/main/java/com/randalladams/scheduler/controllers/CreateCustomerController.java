1687e9c15d3cb57b045abae29150bae0b5f970ef
Randall Adams <rjadams96@gmail.com>
2021-12-05 10:57:26 -0700

    building out customer form and adding observable list for first level divisions

--- src/main/java/com/randalladams/scheduler/controllers/CreateCustomerController.java
+++ src/main/java/com/randalladams/scheduler/controllers/CreateCustomerController.java
@@ -1,22 +1,42 @@
 package com.randalladams.scheduler.controllers;
 
 import com.randalladams.scheduler.model.Customer;
+import com.randalladams.scheduler.model.FirstLevelDivision;
 import com.randalladams.scheduler.services.CustomerService;
+import com.randalladams.scheduler.services.FirstLevelDivisionsService;
 import com.randalladams.scheduler.util.Database;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-public class CreateCustomerController {
+public class CreateCustomerController implements Initializable {
+
+  @FXML
+  private ChoiceBox<FirstLevelDivision> firstLevelDivisionChoiceBox;
 
   /**
-   * Initializer for login scene
+   * Initializer for create customer modal
    * @param url - the url (required for all initializers)
    * @param resourceBundle - the resource bundle for the scene
    */
   public void initialize(URL url, ResourceBundle resourceBundle) {
+    FirstLevelDivisionsService fldService = new FirstLevelDivisionsService();
+    try {
+      ObservableList<FirstLevelDivision> firstLevelDivisions = fldService.getAllFirstLevelDivisions();
+      firstLevelDivisionChoiceBox.setItems(firstLevelDivisions);
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    }
+  }
+
+  public void submitCustomer() {
+    System.out.println("submit");
   }
 }
